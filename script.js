207971a99190b24cdef895dc2f553d70b00e5389
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-07 18:00:06 -0400

    new

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// call this function when body is finished loading
+function init(){
 
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?',
+  function(data) {
+    if (data.stream){
+      document.querySelector('#fcc').innerHTML = 'freecodecamp is currently streaming';
+    }
+    console.log(data);
+
+  });
+
+  // $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/esl_sc2?callback=?', function(data) {
+  //   console.log(data);
+  // });
+
+}
